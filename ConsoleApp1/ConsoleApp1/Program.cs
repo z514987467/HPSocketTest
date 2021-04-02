ec77c29fcf4a29e2cf98b666767517b3ff682b3a
zhangmingzheng <zhangmingzheng@chsdl.com>
2021-04-02 11:21:18 +0800

    init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Autofac;
+using Autofac.Annotation;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            //配置
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            IConfiguration configuration = configBuilder.Build();
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+
+            //日志
+            serviceCollection.AddLogging(builder =>
+            {
+                builder.AddConfiguration(configuration.GetSection("Logging")).AddConsole();
+            });
+
+            var builder = new ContainerBuilder();
+
+            //注入服务器集合
+            builder.Populate(serviceCollection);
+
+            // 注册autofac打标签模式
+            builder.RegisterModule(new AutofacAnnotationModule().SetAllowCircularDependencies(true));
+
+            builder.RegisterBuildCallback(c =>
+            {
+                var logger = c.Resolve<ILogger<Program>>();
+                logger.LogInformation("启动成功！");
+            });
+
+            builder.Build();
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/SocketServer.cs
@@ -0,0 +1,52 @@
+﻿using Autofac.Annotation;
+using HPSocket;
+using HPSocket.Tcp;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    [Component("SocketClient", AutofacScope = AutofacScope.SingleInstance, AutoActivate = true, InitMethod = nameof(SocketClient.Start))]
+    public class SocketClient
+    {
+        [Autowired]
+        private ILogger<SocketClient> logger;
+
+        public void Start()
+        {
+            ITcpClient tcpClient = new TcpClient();
+            // 缓冲区大小,4kb
+            tcpClient.SocketBufferSize = 4096;
+            tcpClient.Async = true;
+            tcpClient.Address = "172.16.9.75";
+            tcpClient.Port = 5013;
+            tcpClient.OnConnect += TcpClient_OnConnect; ;
+            tcpClient.OnClose += TcpClient_OnClose; ;
+            tcpClient.OnReceive += TcpClient_OnReceive; ;
+            tcpClient.KeepAliveInterval = 3000;
+            tcpClient.KeepAliveTime = 3000;
+            tcpClient.Async = true;
+            tcpClient.Connect();
+        }
+
+        private HandleResult TcpClient_OnReceive(IClient sender, byte[] data)
+        {
+            logger.LogDebug("TcpClient_OnReceive");
+            return HandleResult.Ok;
+        }
+
+        private HandleResult TcpClient_OnClose(IClient sender, SocketOperation socketOperation, int errorCode)
+        {
+            logger.LogDebug($"TcpClient_OnClose#socketOperation:{socketOperation}");
+            return HandleResult.Ok;
+        }
+
+        private HandleResult TcpClient_OnConnect(IClient sender)
+        {
+            logger.LogDebug("TcpClient_OnConnect");
+            return HandleResult.Ok;
+        }
+    }
+}
